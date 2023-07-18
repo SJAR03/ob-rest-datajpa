c115b99c5244f3babcd6c142f5e4d3c731231184
72574282+SJAR03@users.noreply.github.com <72574282+SJAR03@users.noreply.github.com>
2023-07-18 00:10:57 -0600

    Entity and repository

--- src/main/java/com/example/obrestdatajpa/ObRestDatajpaApplication.java
+++ src/main/java/com/example/obrestdatajpa/ObRestDatajpaApplication.java
@@ -2,12 +2,38 @@ package com.example.obrestdatajpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class ObRestDatajpaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ObRestDatajpaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(ObRestDatajpaApplication.class, args);
+        BookRepository repository = context.getBean(BookRepository.class);
+
+        // CRUD (Debería ir en un service pero para simplificar la práctica
+        // lo hago acá)
+
+        // crear un libro
+        Book book1 = new Book(null, "IT", "Stephen King", 1280, 1120.00, LocalDate.of(1986, 9, 15), true);
+        Book book2 = new Book(null, "Pet sematary", "Stephen King", 845,880.00, LocalDate.of(1983, 11, 14), true);
+
+        // almacenar un libro
+        System.out.println("Número de libros en base de datos: " + repository.findAll().size());
+
+        repository.save(book1);
+        repository.save(book2);
+
+        // recuperar todos los libros
+        /*repository.findAll();*/
+        System.out.println("Número de libros en base de datos: " + repository.findAll().size());
+
+        // borrar un libro
+        repository.deleteById(1L);
+
+        System.out.println("Número de libros en base de datos: " + repository.findAll().size());
+    }
 
 }
